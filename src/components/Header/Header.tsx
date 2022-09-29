9140578cff461d6d5ac24f22e8548457822dc4ab
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2022-09-29 12:34:19 +0100

    🎨🛠 Header and countdown design fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 const Header = () => {
 	const [showMenu, setShowMenu] = useState(false);
 	const smallScreen = useMediaQuery("(max-width: 600px)");
+	const mediumScreen = useMediaQuery("(max-width: 900px)");
 
 	const toggleMenu = () => {
 		setShowMenu(prevState => !prevState);
@@ -18,19 +19,19 @@ const Header = () => {
 				<div className='absolute top-[10%] right-[5%] -z-10 h-[60vw] max-h-[409px] w-[40vw] max-w-[364px] bg-secondary-main md:right-0 md:top-0 md:h-[28vw] md:w-[25vw]' />
 
 				<nav className='flex w-full items-center justify-between p-5 pb-10'>
-					<figure className='aspect-[157/49] w-[10vw] min-w-[105px]'>
+					<figure className='aspect-[157/49] w-[11.5vw] min-w-[105px] max-w-[200px]'>
 						<LazyLoadImage
-							src='/images/logo.png'
+							src='/images/logo.svg'
 							className='h-full w-full object-contain'
 						/>
 					</figure>
 
 					<a
 						href='/'
-						className='hidden items-center gap-2 text-xl font-medium text-primary-main md:flex'
+						className='hidden items-center gap-2 text-xl font-medium text-primary-main md:flex lg:mt-4'
 					>
 						<span>Register Now</span>
-						<Icon icon='akar-icons:arrow-right' className='text-2xl' />
+						<Icon icon='akar-icons:arrow-right' className='text-3xl' />
 					</a>
 
 					<button onClick={toggleMenu} className='block md:hidden'>
@@ -38,7 +39,7 @@ const Header = () => {
 					</button>
 				</nav>
 
-				<div className='gradient-bg z-20 mx-5 mt-10 h-[550px] min-h-[550px] w-[80%] bg-orange-400 md:relative md:z-10 md:mx-0 md:mt-0 md:h-auto md:max-h-[350px] md:min-h-[30vw] md:w-[87%]'>
+				<div className='gradient-bg z-20 mx-5 mt-10 h-[550px] min-h-[550px] w-[80%] bg-orange-400 md:z-10 md:mx-0 md:mt-0 md:h-[25vw] md:max-h-[400px] md:min-h-[360px] md:w-[87%]'>
 					<div className='z-20 flex h-full flex-col justify-between gap-10 p-5 md:h-auto md:min-h-0 md:justify-center md:py-12 md:px-10 lg:px-24 xl:px-36'>
 						<h1 className='z-50 font-quilon text-[2.5rem] font-medium uppercase leading-[2rem] text-white md:z-0 md:text-7xl md:font-semibold md:leading-[3.2rem]'>
 							Reboot <br />
@@ -46,7 +47,7 @@ const Header = () => {
 							2022
 						</h1>
 
-						<h2 className='font-quilon text-2xl font-medium leading-[1.5rem] text-secondary-main md:max-w-none md:text-4xl md:font-normal md:leading-[2rem]'>
+						<h2 className='font-quilon text-2xl font-medium leading-[1.5rem] text-secondary-main md:max-w-none md:text-[2.1rem] md:font-normal md:leading-[2rem]'>
 							{smallScreen ? (
 								<>
 									Get ready <br />
@@ -60,13 +61,13 @@ const Header = () => {
 						</h2>
 					</div>
 
-					<span className='absolute top-[14%] right-[15%] z-[9999] text-right text-[5.5rem] leading-[4.5rem] text-secondary-main sm:top-[14%] sm:right-[17%] md:top-0 md:right-0 md:text-[8rem] md:leading-[6rem] lg:top-[-2%] lg:right-[-1%] lg:text-[10rem] lg:leading-[8rem]'>
+					<span className='absolute top-[14%] right-[15%] z-[9999] text-right text-[5.5rem] leading-[4.5rem] text-secondary-main sm:top-[14%] sm:right-[17%] md:top-[13%] md:right-[12%] md:text-[8rem] md:leading-[6rem] lg:top-[13.5%] lg:right-[12%] lg:text-[10.5rem] lg:leading-[8rem]'>
 						20
 						<br />
 						22
 					</span>
 
-					<figure className='absolute bottom-[-7%] right-[-10%] z-30 aspect-[193/223] w-[30vw] max-w-[200px] md:bottom-[-20%] md:right-[-7%] md:w-[20vw] md:max-w-[150px]'>
+					<figure className='absolute bottom-[21%] right-[7%] z-50 aspect-[193/223] w-[30vw] max-w-[200px] md:bottom-[27%] md:right-[7%] md:w-[20vw] md:max-w-[180px] lg:bottom-[31%] lg:right-[7%]'>
 						<img
 							src='/images/angle.png'
 							alt=''
@@ -75,24 +76,24 @@ const Header = () => {
 					</figure>
 				</div>
 
-				<div className='z-20 flex flex-col items-center justify-between gap-5 py-12 px-10 text-center md:items-start md:gap-10 md:text-left lg:justify-center lg:px-24 xl:px-36'>
+				<div className='z-20 flex flex-col items-center justify-between gap-5 py-10 px-10 pt-14 text-center md:items-start md:gap-10 md:pt-10 md:text-left lg:justify-center lg:px-24 xl:px-36'>
 					<h3 className='text-xl font-semibold leading-[2rem] text-primary-main md:text-3xl'>
 						DEC. 9TH-11TH 2022 <br />
 						KONGA PLACE, LEKKI, LAGOS
 					</h3>
 
-					<div>
-						<button className='flex w-full items-center gap-2 rounded-full bg-primary-main py-4 px-7 font-medium text-white md:w-auto md:text-xl'>
+					<div className='z-50'>
+						<button className='flex w-full items-center gap-2 rounded-full bg-primary-main py-3 px-7 font-medium text-white md:w-auto md:text-xl'>
 							<span>Register & Book Accomodation</span>
 							<Icon
 								icon='akar-icons:arrow-right'
-								className='text-xl md:text-2xl'
+								className='text-xl md:text-3xl'
 							/>
 						</button>
 					</div>
 				</div>
 
-				<figure className='effect absolute top-[9%] left-[0] z-20 aspect-[384/500] w-[110vw] max-w-[440px] md:top-[-13%] md:left-[35%] md:z-30 md:hidden md:aspect-[695/877] md:w-[60vw] md:max-w-[770px] lg:block lg:w-[52%]'>
+				<figure className='effect absolute top-[9%] left-[0] z-20 aspect-[384/500] w-[110vw] max-w-[440px] md:top-[-13%] md:left-[35%] md:z-30 md:hidden md:aspect-[695/877] md:w-[60vw] md:max-w-[666px] lg:top-[-11%] lg:block lg:w-[50.5%]'>
 					<img
 						src={smallScreen ? "/images/effect-2.png" : "/images/effect.png"}
 						alt=''
@@ -100,15 +101,17 @@ const Header = () => {
 					/>
 				</figure>
 
-				<figure className='absolute top-[22%] left-[23%] z-30 aspect-[407/782] w-[60vw] max-w-[250px] md:top-[7.5%] md:left-[43%] md:w-[30vw] md:max-w-[360px] xl:w-[35%]'>
+				<figure className='absolute top-[22%] left-[23%] z-30 aspect-[407/782] w-[60vw] max-w-[250px] md:top-[7.5%] md:left-[43%] md:w-[30vw] md:max-w-[360px] xl:left-[45.5%] xl:w-[35%] xl:max-w-[370px]'>
 					<img
-						src='/images/pst-iren.png'
+						src={
+							mediumScreen ? "/images/pst-iren.png" : "/images/pst-iren-2.png"
+						}
 						alt=''
 						className='h-full w-full object-contain'
 					/>
 				</figure>
 
-				<figure className='absolute bottom-[8%] left-[-30%] z-0 aspect-square w-[80vw] max-w-[500px] md:bottom-[15%] md:left-[-20%] md:z-30 md:hidden md:w-[60vw] lg:left-[-18%] lg:block xl:left-[-15%]'>
+				<figure className='absolute bottom-[8%] left-[-30%] z-0 aspect-square w-[80vw] max-w-[500px] md:bottom-[13%] md:left-[-20%] md:z-30 md:hidden md:w-[60vw] lg:left-[-18%] lg:block xl:left-[-15%]'>
 					<img
 						src='/images/Blur.png'
 						alt=''
@@ -116,7 +119,7 @@ const Header = () => {
 					/>
 				</figure>
 
-				<figure className='absolute bottom-[-5%] z-30 hidden aspect-square w-[60vw] max-w-[500px] lg:right-[-18%] lg:block xl:right-[-15%]'>
+				<figure className='absolute bottom-[-5%] z-30 hidden aspect-square w-[60vw] max-w-[500px] lg:bottom-[-7%] lg:right-[-18%] lg:block xl:right-[-15%]'>
 					<img
 						src='/images/Blur2.png'
 						alt=''
