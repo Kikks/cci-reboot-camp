5419f29f01d5262f039ebf05a030977681e2a7c0
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2022-09-30 17:49:07 +0100

    🏞 Image Gallery fix

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,103 +4,199 @@ import { Star1 } from "../../svg/star1";
 import { Star2 } from "../../svg/star2";
 import { Star4 } from "../../svg/star4";
 import { Star3 } from "../../svg/star3";
-import { Play } from "../../svg/play";
-import { ArrowRight } from "../../svg/arrowRight";
+// import { Play } from "../../svg/play";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Icon } from "@iconify/react";
+import { useEffect, useState } from "react";
 
 const Gallery = () => {
-  const bigScreen = useMediaQuery("(min-width: 768px)");
+	const bigScreen = useMediaQuery("(min-width: 768px)");
+	const mediumScreen = useMediaQuery("(max-width: 1024px)");
+	const [showBackArrow, setShowBackArrow] = useState(false);
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const [showModal, setShowModal] = useState(false);
 
-  return (
-    <section className="gallery-gradient px-6 md:px-8">
-      <div className="relative flex items-center pt-4 md:py-4">
-        <span className="hidden md:relative md:left-[8%] md:block md:h-[50px] md:w-[40px]">
-          {Star4}
-        </span>
-        <span className="h-[28px] w-[22px] md:absolute md:left-[12%] md:top-[70%] md:h-[24px] md:w-[19px]">
-          {Star1}
-        </span>
-        <span className="absolute left-[8%] top-[10%] h-[34px] w-[26px] md:left-[15%] md:top-[15%] md:h-[86.28px] md:w-[67px]">
-          {Star2}
-        </span>
-        <span className="absolute right-[20%] h-[12px] w-[15px] md:h-[45px] md:w-[36px]">
-          {Star3}
-        </span>
-      </div>
+	useEffect(() => {
+		if (showModal) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "visible";
+		}
 
-      <div className="relative md:mx-auto md:w-max">
-        <h1 className="text-center font-quilon text-[2em] leading-10 text-white lg:text-5xl xl:text-[3.25em]">
-          The Reboot Camp <span className={`${bigScreen ? "inline" : "block"}`}>2022</span>
-        </h1>
-        <span className={`absolute ${bigScreen ? `right-[0]` : `right-[35%]`}`}>{Dash}</span>
-        <span className="absolute right-[10%] bottom-0 block h-[35px] w-[28px] md:hidden">
-          {Star4}
-        </span>
-      </div>
+		return () => {
+			document.body.style.overflow = "visible";
+		};
+	}, [showModal]);
 
-      <div className="mt-10 md:mx-auto md:max-w-[704px] md:px-16 lg:mx-auto lg:mt-12 lg:flex lg:max-w-[1376px] lg:items-center lg:gap-[1em] xl:gap-[1.3em]">
-        <div className="relative">
-          {bigScreen ? (
-            <img
-              src="/images/galleryBig1.png"
-              alt="gallery1"
-              className="block h-full w-full md:mb-[2em] lg:mb-0"
-            />
-          ) : (
-            <img src="/images/gallery1.png" alt="gallery1" className="block h-full w-full" />
-          )}
-          <span className="play">{Play}</span>
-        </div>
+	const toggleShowModal = () => {
+		setShowModal(prevState => !prevState);
+	};
 
-        <div className="grid-block">
-          {bigScreen ? (
-            <img src="/images/galleryBig2.png" alt="gallery2" className="block h-full w-full" />
-          ) : (
-            <img src="/images/gallery2.png" alt="gallery2" className="block h-full w-full" />
-          )}
+	const handleSlideRight = () => {
+		const sliderContainer = document.getElementById(
+			mediumScreen ? "images" : "slider-container"
+		);
 
-          <div className="relative">
-            {bigScreen ? (
-              <img src="/images/galleryBig3.png" alt="gallery3" className="block h-full w-full" />
-            ) : (
-              <img src="/images/gallery3.png" alt="gallery3" className="block h-full w-full" />
-            )}
+		if (sliderContainer) {
+			sliderContainer.scrollLeft += 350;
 
-            <span
-              className={`${
-                bigScreen ? `top-[95%]` : `top-[90%]`
-              } absolute -right-[10px] lg:hidden`}
-            >
-              {ArrowRight}
-            </span>
-          </div>
+			if (sliderContainer.scrollLeft === 0) {
+				setShowBackArrow(false);
+			} else if (!showBackArrow) {
+				setShowBackArrow(true);
+			}
+		}
+	};
 
-          <div className="relative">
-            {bigScreen ? (
-              <img src="/images/galleryBig4.png" alt="gallery4" className="block h-full w-full" />
-            ) : (
-              <img src="/images/gallery4.png" alt="gallery4" className="block h-full w-full" />
-            )}
+	const handleSlideLeft = () => {
+		const sliderContainer = document.getElementById(
+			mediumScreen ? "images" : "slider-container"
+		);
 
-            <span className="absolute top-[95%] -right-[10px] hidden lg:block">{ArrowRight}</span>
-          </div>
+		if (sliderContainer) {
+			sliderContainer.scrollLeft += -350;
 
-          {bigScreen ? (
-            <img src="/images/galleryBig5.png" alt="gallery5" className="block h-full w-full" />
-          ) : (
-            <img src="/images/gallery5.png" alt="gallery5" className="block h-full w-full" />
-          )}
+			if (sliderContainer.scrollLeft === 0) {
+				setShowBackArrow(false);
+			} else if (!showBackArrow) {
+				setShowBackArrow(true);
+			}
+		}
+	};
 
-          {bigScreen && (
-            <>
-              <img src="/images/galleryBig6.png" alt="gallery6" className="block h-full w-full" />
-              <img src="/images/galleryBig7.png" alt="gallery7" className="block h-full w-full" />
-            </>
-          )}
-        </div>
-      </div>
-    </section>
-  );
+	const handleImageclicked = (index: number) => {
+		setSelectedIndex(index);
+		toggleShowModal();
+	};
+
+	return (
+		<>
+			<section className='gallery-gradient'>
+				<div className='relative flex items-center pt-4 md:py-4'>
+					<span className='hidden md:relative md:left-[8%] md:block md:h-[50px] md:w-[40px]'>
+						{Star4}
+					</span>
+					<span className='h-[28px] w-[22px] md:absolute md:left-[12%] md:top-[70%] md:h-[24px] md:w-[19px]'>
+						{Star1}
+					</span>
+					<span className='absolute left-[8%] top-[10%] h-[34px] w-[26px] md:left-[15%] md:top-[15%] md:h-[86.28px] md:w-[67px]'>
+						{Star2}
+					</span>
+					<span className='absolute right-[20%] h-[12px] w-[15px] md:h-[45px] md:w-[36px]'>
+						{Star3}
+					</span>
+				</div>
+
+				<div className='relative md:mx-auto md:w-max'>
+					<h1 className='text-center font-quilon text-[2em] leading-10 text-white lg:text-5xl xl:text-[3.25em]'>
+						The Reboot Camp{" "}
+						<span className={`${bigScreen ? "inline" : "block"}`}>2022</span>
+					</h1>
+					<span
+						className={`absolute ${bigScreen ? `right-[0]` : `right-[35%]`}`}
+					>
+						{Dash}
+					</span>
+					<span className='absolute right-[10%] bottom-0 block h-[35px] w-[28px] md:hidden'>
+						{Star4}
+					</span>
+				</div>
+
+				<div className='no-scrollbar relative mt-10 w-full overflow-x-hidden px-5 sm:mx-auto sm:max-w-[500px] lg:mx-auto lg:mt-12 lg:flex lg:max-w-[1500px] lg:items-center lg:gap-[1em] lg:px-7 xl:gap-[1.3em]'>
+					<div
+						id='slider-container'
+						className='no-scrollbar flex w-full flex-col items-center gap-3 md:gap-5 lg:flex-row lg:overflow-x-auto'
+					>
+						<div className='relative'>
+							<figure
+								onClick={() => handleImageclicked(46)}
+								className='block aspect-[589/504] min-h-[290px] w-full overflow-hidden rounded-xl bg-black/20 md:min-h-[390px] lg:min-h-0 lg:w-[41vw] lg:max-w-[560px]'
+							>
+								<LazyLoadImage
+									src={
+										mediumScreen
+											? "/images/gallery/46-preview-small-screen.jpg"
+											: "/images/gallery/46-preview-large-screen.jpg"
+									}
+									alt=''
+									className='undraggable h-full w-full object-cover'
+								/>
+							</figure>
+							{/* <span className='play hidden'>{Play}</span> */}
+						</div>
+
+						<div
+							id='images'
+							className={`grid-stack w-full place-items-center gap-3 md:gap-5 lg:w-auto ${
+								mediumScreen ? "overflow-x-auto" : ""
+							}`}
+						>
+							{Array.from(Array(46).keys()).map(number => (
+								<figure
+									onClick={() => handleImageclicked(number + 1)}
+									key={number}
+									className='aspect-square h-full w-[42vw] max-w-[180px] overflow-hidden rounded-xl bg-black/20 md:max-w-[220px] lg:w-[17vw] lg:max-w-[230px]'
+								>
+									<LazyLoadImage
+										src={`/images/gallery/${number + 1}-preview.jpg`}
+										alt=''
+										className='undraggable h-full w-full object-cover'
+									/>
+								</figure>
+							))}
+						</div>
+					</div>
+
+					{showBackArrow && (
+						<button
+							onClick={handleSlideLeft}
+							className='absolute top-[70%] left-3 grid h-10 w-10 place-items-center rounded-full border border-[#131313] bg-white md:h-14 md:w-14 lg:top-[50%] lg:-translate-y-[50%]'
+						>
+							<Icon
+								icon='carbon:chevron-left'
+								className='text-xl text-[#131313] lg:text-2xl'
+							/>
+						</button>
+					)}
+
+					<button
+						onClick={handleSlideRight}
+						className='absolute top-[70%] right-3 grid h-10 w-10 place-items-center rounded-full border border-[#131313] bg-white md:h-14 md:w-14 lg:top-[50%] lg:-translate-y-[50%]'
+					>
+						<Icon
+							icon='carbon:chevron-right'
+							className='text-xl text-[#131313] lg:text-2xl'
+						/>
+					</button>
+				</div>
+			</section>
+
+			{showModal && (
+				<div className='fixed top-0 left-0 z-[10000000] h-screen w-screen bg-black/80 '>
+					<figure className='relative grid h-full w-full place-items-center'>
+						<div
+							className='absolute top-0 left-0 h-full w-full'
+							onClick={toggleShowModal}
+						/>
+
+						<LazyLoadImage
+							src={`/images/gallery/${selectedIndex}.jpg`}
+							alt=''
+							className='undraggable full-screen-image z-10'
+						/>
+					</figure>
+
+					<button
+						className='absolute right-3 top-3 grid h-10 w-10 place-items-center rounded-full bg-white lg:top-5 lg:right-10'
+						onClick={toggleShowModal}
+					>
+						<Icon icon='carbon:close' className='text-xl text-red-500' />
+					</button>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Gallery;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,124 +5,151 @@ import useMediaQuery from "../../hooks/useMediaQuery";
 import { useState } from "react";
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const smallScreen = useMediaQuery("(max-width: 600px)");
-  const mediumScreen = useMediaQuery("(max-width: 900px)");
+	const [showMenu, setShowMenu] = useState(false);
+	const smallScreen = useMediaQuery("(max-width: 600px)");
+	const mediumScreen = useMediaQuery("(max-width: 900px)");
 
-  const toggleMenu = () => {
-    setShowMenu((prevState) => !prevState);
-  };
+	const toggleMenu = () => {
+		setShowMenu(prevState => !prevState);
+	};
 
-  return (
-    <>
-      <header className="relative h-auto min-h-screen w-full overflow-hidden overflow-x-hidden md:h-auto md:max-h-[900px] md:min-h-0 lg:h-screen lg:min-h-screen">
-        <div className="absolute top-[10%] right-[5%] -z-10 h-[60vw] max-h-[409px] w-[40vw] max-w-[364px] bg-secondary-main md:right-0 md:top-0 md:h-[28vw] md:w-[25vw]" />
+	return (
+		<>
+			<header className='relative h-auto min-h-screen w-full overflow-hidden overflow-x-hidden md:h-auto md:max-h-[900px] md:min-h-0 lg:h-screen lg:min-h-screen'>
+				<div className='absolute top-[10%] right-[5%] -z-10 h-[60vw] max-h-[409px] w-[40vw] max-w-[364px] bg-secondary-main md:right-0 md:top-0 md:h-[28vw] md:w-[25vw]' />
 
-        <nav className="flex w-full items-center justify-between p-5 pb-10">
-          <figure className="aspect-[157/49] w-[11.5vw] min-w-[105px] max-w-[200px]">
-            <LazyLoadImage src="/images/logo.svg" className="h-full w-full object-contain" />
-          </figure>
+				<nav className='flex w-full items-center justify-between p-5 pb-10'>
+					<figure className='aspect-[157/49] w-[11.5vw] min-w-[105px] max-w-[200px]'>
+						<LazyLoadImage
+							src={
+								smallScreen
+									? "/images/logo-small-screen.svg"
+									: "/images/logo.svg"
+							}
+							className='undraggable h-full w-full object-contain'
+						/>
+					</figure>
 
-          <a
-            href="/"
-            className="hidden items-center gap-2 text-xl font-medium text-primary-main md:flex lg:mt-4"
-          >
-            <span>Register Now</span>
-            <Icon icon="akar-icons:arrow-right" className="text-3xl" />
-          </a>
+					<a
+						href='/'
+						className='hidden items-center gap-2 text-xl font-medium text-primary-main md:flex lg:mt-4'
+					>
+						<span>Register Now</span>
+						<Icon icon='akar-icons:arrow-right' className='text-3xl' />
+					</a>
 
-          <button onClick={toggleMenu} className="block md:hidden">
-            <Icon icon="gg:menu-right" className="text-3xl text-[#131313]" />
-          </button>
-        </nav>
+					<button onClick={toggleMenu} className='block md:hidden'>
+						<Icon icon='gg:menu-right' className='text-3xl text-[#131313]' />
+					</button>
+				</nav>
 
-        <div className="gradient-bg z-20 mx-5 mt-10 h-[550px] min-h-[550px] w-[80%] bg-orange-400 md:z-10 md:mx-0 md:mt-0 md:h-[25vw] md:max-h-[400px] md:min-h-[360px] md:w-[87%]">
-          <div className="z-20 flex h-full flex-col justify-between gap-10 p-5 md:h-auto md:min-h-0 md:justify-center md:py-12 md:px-10 lg:px-24 xl:px-36">
-            <h1 className="z-50 font-quilon text-[2.5rem] font-medium uppercase leading-[2rem] text-white md:z-0 md:text-7xl md:font-semibold md:leading-[3.2rem]">
-              Reboot <br />
-              Camp <br />
-              2022
-            </h1>
+				<div className='gradient-bg z-20 mx-5 mt-10 h-[550px] min-h-[550px] w-[80%] bg-orange-400 md:z-10 md:mx-0 md:mt-0 md:h-[25vw] md:max-h-[400px] md:min-h-[360px] md:w-[87%]'>
+					<div className='z-20 flex h-full flex-col justify-between gap-10 p-5 md:h-auto md:min-h-0 md:justify-center md:py-12 md:px-10 lg:px-24 xl:px-36'>
+						<h1 className='z-50 font-quilon text-[2.5rem] font-medium uppercase leading-[2rem] text-white md:z-0 md:text-7xl md:font-semibold md:leading-[3.2rem]'>
+							Reboot <br />
+							Camp <br />
+							2022
+						</h1>
 
-            <h2 className="font-quilon text-2xl font-medium leading-[1.5rem] text-secondary-main md:max-w-none md:text-[2.1rem] md:font-normal md:leading-[2rem]">
-              {smallScreen ? (
-                <>
-                  Get ready <br />
-                  for a R.I.C.H <br /> Experience!
-                </>
-              ) : (
-                <>
-                  Get ready for a <br /> R.I.C.H Experience!
-                </>
-              )}
-            </h2>
-          </div>
+						<h2 className='font-quilon text-2xl font-medium leading-[1.5rem] text-secondary-main md:max-w-none md:text-[2.1rem] md:font-normal md:leading-[2rem]'>
+							{smallScreen ? (
+								<>
+									Get ready <br />
+									for a R.I.C.H <br /> Experience!
+								</>
+							) : (
+								<>
+									Get ready for a <br /> R.I.C.H Experience!
+								</>
+							)}
+						</h2>
+					</div>
 
-          <span className="absolute top-[14%] right-[15%] z-[9999] text-right text-[5.5rem] leading-[4.5rem] text-secondary-main sm:top-[14%] sm:right-[17%] md:top-[13%] md:right-[12%] md:text-[8rem] md:leading-[6rem] lg:top-[13.5%] lg:right-[12%] lg:text-[10.5rem] lg:leading-[8rem]">
-            20
-            <br />
-            22
-          </span>
+					<span className='absolute top-[14%] right-[15%] z-[9999] text-right text-[5.5rem] leading-[4.5rem] text-secondary-main sm:top-[14%] sm:right-[17%] md:top-[13%] md:right-[12%] md:text-[8rem] md:leading-[6rem] lg:top-[13.5%] lg:right-[12%] lg:text-[10.5rem] lg:leading-[8rem]'>
+						20
+						<br />
+						22
+					</span>
 
-          <figure className="angle absolute bottom-[21%] right-[7%] z-50 aspect-[193/223] w-[30vw] max-w-[200px] md:bottom-[27%] md:right-[7%] md:w-[20vw] md:max-w-[180px] lg:bottom-[28%] lg:right-[7%]">
-            <img src="/images/angle.png" alt="" className="h-full w-full object-contain" />
-          </figure>
-        </div>
+					<figure className='angle absolute bottom-[21%] right-[7%] z-50 aspect-[193/223] w-[30vw] max-w-[200px] md:bottom-[27%] md:right-[7%] md:w-[20vw] md:max-w-[180px] lg:bottom-[28%] lg:right-[7%]'>
+						<img
+							src='/images/angle.png'
+							alt=''
+							className='undraggable h-full w-full object-contain'
+						/>
+					</figure>
+				</div>
 
-        <div className="z-20 flex flex-col items-center justify-between gap-5 py-10 px-10 pt-14 text-center md:items-start md:gap-10 md:pt-10 md:text-left lg:justify-center lg:px-24 xl:px-36">
-          <h3 className="text-xl font-semibold leading-[2rem] text-primary-main md:text-3xl">
-            DEC. 9TH-11TH 2022 <br />
-            KONGA PLACE, LEKKI, LAGOS
-          </h3>
+				<div className='z-20 flex flex-col items-center justify-between gap-5 py-10 px-10 pt-14 text-center md:items-start md:gap-10 md:pt-10 md:text-left lg:justify-center lg:px-24 xl:px-36'>
+					<h3 className='text-xl font-semibold leading-[2rem] text-primary-main md:text-3xl'>
+						DEC. 9TH-11TH 2022 <br />
+						KONGA PLACE, LEKKI, LAGOS
+					</h3>
 
-          <div className="z-50">
-            <button className="flex w-full items-center gap-2 rounded-full bg-primary-main py-3 px-7 font-medium text-white md:w-auto md:text-xl">
-              <span>Register & Book Accomodation</span>
-              <Icon icon="akar-icons:arrow-right" className="text-xl md:text-3xl" />
-            </button>
-          </div>
-        </div>
+					<div className='z-50'>
+						<button className='flex w-full items-center gap-2 rounded-full bg-primary-main py-3 px-7 font-medium text-white md:w-auto md:text-xl'>
+							<span>Register & Book Accomodation</span>
+							<Icon
+								icon='akar-icons:arrow-right'
+								className='text-xl md:text-3xl'
+							/>
+						</button>
+					</div>
+				</div>
 
-        <figure className="effect absolute top-[9%] left-[0] z-20 aspect-[384/500] w-[110vw] max-w-[440px] md:top-[-13%] md:left-[35%] md:z-30 md:hidden md:aspect-[695/877] md:w-[60vw] md:max-w-[666px] lg:top-[-11%] lg:block lg:w-[50.5%]">
-          <img
-            src={smallScreen ? "/images/effect-2.png" : "/images/effect.png"}
-            alt=""
-            className="h-full w-full object-contain"
-          />
-        </figure>
+				<figure className='effect absolute top-[9%] left-[0] z-20 aspect-[384/500] w-[110vw] max-w-[440px] md:top-[-13%] md:left-[35%] md:z-30 md:hidden md:aspect-[695/877] md:w-[60vw] md:max-w-[666px] lg:top-[-11%] lg:block lg:w-[50.5%]'>
+					<img
+						src={smallScreen ? "/images/effect-2.png" : "/images/effect.png"}
+						alt=''
+						className='undraggable h-full w-full object-contain'
+					/>
+				</figure>
 
-        <figure className="absolute top-[22%] left-[23%] z-30 aspect-[407/782] w-[60vw] max-w-[250px] md:top-[7.5%] md:left-[43%] md:w-[30vw] md:max-w-[360px] xl:left-[45.5%] xl:w-[35%] xl:max-w-[370px]">
-          <img
-            src={mediumScreen ? "/images/pst-iren.png" : "/images/pst-iren-2.png"}
-            alt=""
-            className="h-full w-full object-contain"
-          />
-        </figure>
+				<figure className='absolute top-[22%] left-[23%] z-30 aspect-[407/782] w-[60vw] max-w-[250px] md:top-[7.5%] md:left-[43%] md:w-[30vw] md:max-w-[360px] xl:left-[45.5%] xl:w-[35%] xl:max-w-[370px]'>
+					<img
+						src={
+							mediumScreen ? "/images/pst-iren.png" : "/images/pst-iren-2.png"
+						}
+						alt=''
+						className='undraggable h-full w-full object-contain'
+					/>
+				</figure>
 
-        <figure className="absolute bottom-[8%] left-[-30%] z-0 aspect-square w-[80vw] max-w-[500px] md:bottom-[13%] md:left-[-20%] md:z-30 md:hidden md:w-[60vw] lg:left-[-18%] lg:block xl:left-[-15%]">
-          <img src="/images/Blur.png" alt="" className="h-full w-full object-contain" />
-        </figure>
+				<figure className='absolute bottom-[8%] left-[-30%] z-0 aspect-square w-[80vw] max-w-[500px] md:bottom-[13%] md:left-[-20%] md:z-30 md:hidden md:w-[60vw] lg:left-[-18%] lg:block xl:left-[-15%]'>
+					<img
+						src='/images/Blur.png'
+						alt=''
+						className='undraggable h-full w-full object-contain'
+					/>
+				</figure>
 
-        <figure className="absolute bottom-[-5%] z-30 hidden aspect-square w-[60vw] max-w-[500px] lg:bottom-[-7%] lg:right-[-18%] lg:block xl:right-[-15%]">
-          <img src="/images/Blur2.png" alt="" className="h-full w-full object-contain" />
-        </figure>
-      </header>
+				<figure className='absolute bottom-[-5%] z-30 hidden aspect-square w-[60vw] max-w-[500px] lg:bottom-[-7%] lg:right-[-18%] lg:block xl:right-[-15%]'>
+					<img
+						src='/images/Blur2.png'
+						alt=''
+						className='undraggable h-full w-full object-contain'
+					/>
+				</figure>
+			</header>
 
-      {showMenu && (
-        <div className="fixed top-0 left-0 z-[1000000] h-screen w-screen bg-black/70 px-5 py-16 text-[#131313]">
-          <div className="absolute top-0 left-0 h-full w-full" onClick={toggleMenu} />
+			{showMenu && (
+				<div className='fixed top-0 left-0 z-[1000000] h-screen w-screen bg-black/70 px-5 py-16 text-[#131313]'>
+					<div
+						className='absolute top-0 left-0 h-full w-full'
+						onClick={toggleMenu}
+					/>
 
-          <a
-            href="/"
-            className="flex w-full items-center justify-center gap-2 rounded-xl bg-white py-5"
-          >
-            <span>Register & Book Accommodation</span>
-            <Icon icon="akar-icons:arrow-right" className="text-2xl" />
-          </a>
-        </div>
-      )}
-    </>
-  );
+					<a
+						href='/'
+						className='flex w-full items-center justify-center gap-2 rounded-xl bg-white py-5'
+					>
+						<span>Register & Book Accommodation</span>
+						<Icon icon='akar-icons:arrow-right' className='text-2xl' />
+					</a>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Header;
