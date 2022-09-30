e281ffb87ebf538cf03e0dbb0134d958a6d7df45
vickysomtee <vickysomtee@gmail.com>
2022-09-30 16:57:07 +0100

    fix: experience section

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -2,12 +2,14 @@ import "./Experience.css";
 const Experience = () => {
   return (
     <>
-      <section className="experience-bg w-full p-[30px] sm:p-[100px] ">
-        <div className="rounded-lg bg-[#FFF] p-[25px] sm:p-[50px]">
-          <div>
-            <img className="my-[40px] mx-auto" src="/images/experience_title.png" alt="" />
+      <section className="experience-bg relative w-full p-[30px] sm:p-[100px] ">
+        <div className="rounded-xl bg-[#FFF] p-[25px] sm:p-[100px]">
+          <div className="mb-10">
+            <h1 className="experience-title text-[40px] sm:text-[70px]">
+              The Rich Experience
+            </h1>
           </div>
-          <div className="grid justify-between md:flex">
+          <div className="grid md:flex md:justify-between">
             <div className="w-100% sm-w-[25%] mt-5 flex">
               <img
                 className="relative top-2 h-[30px] w-[30px]"
@@ -18,7 +20,7 @@ const Experience = () => {
                 <h1 className="text-3xl font-bold text-[#21409A]">
                   Rejuvenation
                 </h1>
-                <p className="text-xl">
+                <p className="max-w-[350px] text-xl">
                   The reboot camp is that camp meeting you need to get your
                   prayer life back on track, wax stronger in fervor and
                   revitalize your spiritual devotion to the way it ought to be.
@@ -26,7 +28,7 @@ const Experience = () => {
               </div>
             </div>
 
-            <div className="w-100% sm-w-[40%] sm:ml-[20px] mt-5 flex">
+            <div className="w-100% sm-w-[25%] mt-5 flex">
               <img
                 className=" relative top-2 h-[30px] w-[30px]"
                 src="/icons/bible.svg"
@@ -36,7 +38,7 @@ const Experience = () => {
                 <h1 className="text-3xl font-bold text-[#F36F21]">
                   Indoctrination
                 </h1>
-                <p className="text-xl">
+                <p className="max-w-[350px] text-xl">
                   The word of God is the ultimate standard for doctrine that
                   should instruct us in the way of righteousness. The undiluted
                   word of God will be taught with precision and accuracy.
@@ -56,7 +58,7 @@ const Experience = () => {
                 <h1 className="text-3xl font-bold text-[#ED1C24]">
                   Celebration
                 </h1>
-                <p className="text-xl">
+                <p className="max-w-[350px] text-xl">
                   The reboot camp is that camp meeting you need to get your
                   prayer life back on track, wax stronger in fervor and
                   revitalize your spiritual devotion to the way it ought to be.
@@ -64,7 +66,7 @@ const Experience = () => {
               </div>
             </div>
 
-            <div className="w-100% sm-w-[40%] sm:ml-[20px] mt-5 flex">
+            <div className="w-100% sm-w-[25%] mt-5 flex">
               <img
                 className=" relative top-2 h-[30px] w-[30px]"
                 src="/icons/people.svg"
@@ -74,7 +76,7 @@ const Experience = () => {
                 <h1 className="text-3xl font-bold text-[#2ABAA7]">
                   Harvest of souls
                 </h1>
-                <p className="text-xl">
+                <p className="max-w-[350px] text-xl">
                   The word of God is the ultimate standard for doctrine that
                   should instruct us in the way of righteousness. The undiluted
                   word of God will be taught with precision and accuracy.
@@ -82,6 +84,21 @@ const Experience = () => {
               </div>
             </div>
           </div>
+          <img
+            className="absolute hidden top-[250px] left-[25px] sm:block sm:left-[94px]"
+            src="/images/curly_1.png"
+            alt=""
+          />
+          <img
+            className="absolute bottom-[200px] right-[90px]"
+            src="/images/curly_2.png"
+            alt=""
+          />
+          <img
+            className="absolute block top-[250px] left-[25px] sm:hidden sm:left-[94px]"
+            src="/images/curly_3.png"
+            alt=""
+          />
         </div>
       </section>
     </>
